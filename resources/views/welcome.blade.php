e57fb1dab4547ff39d89b7e3317e8962070ab2e6
vipcristedo <vipcristedo@gmail.com>
2019-12-04 17:48:10 +0700

    test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel-todo</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <ul style="list-style: none; margin: 0; padding: 0;text-align: left;">
+                        <li class="links"><a href="https://laravel.com/docs">Họ và tên:</a><span>Pạm Quang Sáng</span></li>
+                        <li class="links"><a href="https://laravel.com/docs">Năm sinh:</a><span>2000</span></li>
+                        <li class="links"><a href="https://laravel.com/docs">Trường:</a><span>ĐH Bách Khoa Hà Nội</span></li>
+                        <li class="links"><a href="https://laravel.com/docs">Mục tiêu sau khóa học:</a><span>Thực tập sớm, ra trường làm việc lương cao</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
